3d560ce883026c67b27515cf117aee64951a6d39
Mesalina23 <mebrapu23@gmail.com>
2021-09-15 23:37:31 +0200

    fix shareCollapsable

--- src/js/04share.js
+++ src/js/04share.js
@@ -16,6 +16,7 @@ function createCard() {
       console.log(pepino);
       pepinoLink = pepino.cardURL;
       if (pepino.success === true) {
+        notCreated.classList.remove('hidden');
         urlCard.innerHTML = `<a href="${pepino.cardURL}">Pincha aquí, ${data.name} <i class="far fa-smile-wink"></i> </a>`;
         twitterBtn.classList.remove('hidden');
         twitterBtn.innerHTML = `<a class= "twitter__link js_twitterLink" href="https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&url=${pepinoLink}" target="_blank"><i class="fab fa-twitter"></i> Compartir en Twiter</a>`;
