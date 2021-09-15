2f5e7cc4f0fc09ae35609104b0c8d569dc0f31ba
CriSFV <cristina.f.valverde@gmail.com>
2021-09-15 22:07:50 +0200

    twitterShare

--- src/js/04share.js
+++ src/js/04share.js
@@ -1,8 +1,9 @@
 'use strict';
-/*aquí empieza el share*/
 const createButton = document.querySelector('.js_shareButton');
 const urlCard = document.querySelector('.js_urlCard');
 const notCreated = document.querySelector('.js_notCreate');
+const twitterBtn = document.querySelector('.js_twiterBtn');
+let pepinoLink;
 function createCard() {
   fetch('https://awesome-profile-cards.herokuapp.com/card', {
     method: 'POST',
@@ -14,18 +15,21 @@ function createCard() {
     .then((response) => response.json())
     .then((pepino) => {
       console.log(pepino);
+      pepinoLink = pepino.cardURL;
       if (pepino.success === true) {
         urlCard.innerHTML = `<a href="${pepino.cardURL}">Pincha aquí, ${data.name} <i class="far fa-smile-wink"></i> </a>`;
+        twitterBtn.classList.remove('hidden');
+        twitterBtn.innerHTML = `<a class= "twitter__link js_twitterLink" href="https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&url=${pepinoLink}" target="_blank"><i class="fab fa-twitter"></i> Compartir en Twiter</a>`;
       } else {
         notCreated.innerHTML = 'No hemos podido crear la tarjeta';
         urlCard.innerHTML = `<i class="far fa-sad-tear"></i> Introduzca todos los campos obligatorios`;
       }
     });
 }
-// sandra: ese console.log nos da un array con 2 atrib: cardURL y success: true. si copiamos el contenido del atrib cardURL y lo pegamos al navegador nos redirecciona a la tarjeta del usuario, con enlaces funcionando, foto, todo.
-//   console.log('decidir que hacer con los datos: pintar url en el html');
-// ejecutar una función: si success === true (sandra: o si data.cardURL === true) la pinto (sandra: la pinto en cardResultElement.innerHTML = data.cardURL) y pinto el botton de twiter. Si genero la tarjeta con false (else), da error y pinta undefined (sandra: podemos añadir un mensaje para el usuario que diga "rellena los campos requeridos")
-// })
+function handleTwitter() {
+  twitterBtn.href = `"https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&url=${pepinoLink}"`;
+}
+twitterBtn.addEventListener('click', handleTwitter);
 
 function handleCreateBtn(ev) {
   ev.preventDefault();
